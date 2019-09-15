94ae4708c632355874c9456c58b2b46a58411ced
Alisa <a.popova9@g.nsu.ru>
2019-09-16 00:15:00 +0700

    Checking on existed user

--- app/routes/users.js
+++ app/routes/users.js
@@ -7,26 +7,33 @@ const User = require('../models/user').User;
 
 router.post('/signup', async (request, response, next) => {
     try {
-        let user;
-        await bcrypt.hash(request.body.password, 10, async (err, hash) => {
-            if (err) {
-                return response.status(500).json({
-                    error: err,
-                });
-            } else {
-                user = new User({
-                    _id: new mongoose.Types.ObjectId(),
-                    email: request.body.email,
-                    password: hash,
-                });
-                const result = await user.save();
-                response.status(201).json({
-                    message: "Handling POST request to /users/signup",
-                    createdUser: result,
-                });
-                console.log(result);
-            }
-        });
+        const doc = await User.find({ email: request.body.email }).exec();
+        if (doc.length >= 1) {
+            return response.status(409).json({
+                message: 'User with such email is already exists',
+            })
+        } else {
+            let user;
+            await bcrypt.hash(request.body.password, 10, async (err, hash) => {
+                if (err) {
+                    return response.status(500).json({
+                        error: err,
+                    });
+                } else {
+                    user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: request.body.email,
+                        password: hash,
+                    });
+                    const result = await user.save();
+                    response.status(201).json({
+                        message: "Handling POST request to /users/signup",
+                        createdUser: result,
+                    });
+                    console.log(result);
+                }
+            });
+        }
     } catch (e) {
         console.log(e);
     }
